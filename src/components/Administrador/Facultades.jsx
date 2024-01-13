10199dc2cdfd0e329c54eece4a8db5d77f5ea8c1
Néstor Chumania <nestor.chumania@epn.edu.ec>
2024-01-12 21:03:43 -0500

    Facultades hasta editar

--- src/components/Administrador/Facultades.jsx
+++ src/components/Administrador/Facultades.jsx
@@ -1,5 +1,9 @@
-import React, { useMemo, useState } from "react";
-import { useTable, useFilters } from "react-table";
+import React, { useMemo, useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useTable, usePagination, useFilters } from "react-table";
+
 import Modal from "react-modal";
 import { useForm } from "react-hook-form";
 import { toast, ToastContainer } from "react-toastify";
@@ -26,12 +30,8 @@ function TextFilter({ column: { filterValue, preFilteredRows, setFilter } }) {
 }
 
 const Facultades = () => {
-  const [facultades, setFacultades] = useState([
-    {
-      edificio: "Facultad 1",
-      nombre: "Esfot",
-    },
-  ]);
+  const [facultades, setFacultades] = useState([]);
+  const [edificios, setEdificios] = useState([]); // Nuevo estado para almacenar la lista de edificios
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalEditarIsOpen, setModalEditarIsOpen] = useState(false);
@@ -44,20 +44,63 @@ const Facultades = () => {
     reset,
   } = useForm();
 
-  const onSubmit = (data) => {
-    Swal.fire({
-      title: "¿Estás seguro de agregar esta Facultad?",
-      showDenyButton: true,
-      confirmButtonText: `Continuar`,
-      denyButtonText: `Cancelar`,
-    }).then((result) => {
+  const onSubmit = async (data) => {
+    const buildingId = data.edificio;
+    const payload = {
+      buildingId,
+      name: data.nombre,
+      description: data.descripcion,
+    };
+
+    try {
+      setLoadingFacultad(true);
+      const token = Cookies.get("token");
+
+      const existingFaculty = facultades.find(
+        (faculty) => faculty.name === data.nombre
+      );
+      if (existingFaculty) {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Ya existe una facultad con ese nombre",
+        });
+        return;
+      }
+
+      const result = await Swal.fire({
+        title: "¿Estás seguro de agregar esta Facultad?",
+        showDenyButton: true,
+        confirmButtonText: `Continuar`,
+        denyButtonText: `Cancelar`,
+      });
+
       if (result.isConfirmed) {
-        setFacultades([...facultades, data]);
+        const response = await axios.post(
+          `${process.env.REACT_APP_API_URL}/faculties`,
+          payload,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        setFacultades([...facultades, response.data]);
         setModalIsOpen(false);
         toast.success("Facultad agregada exitosamente!");
         reset();
+        await fetchFaculties(token);
       }
-    });
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "Error al agregar la facultad",
+      });
+    } finally {
+      setLoadingFacultad(false);
+    }
   };
 
   const editarFacultad = (index) => {
@@ -84,18 +127,130 @@ const Facultades = () => {
       }
     });
   };
-  const eliminarFacultad = (index) => {
-    Swal.fire({
-      title: "¿Estás seguro de eliminar esta Facultad?",
-      showDenyButton: true,
-      confirmButtonText: `Eliminar`,
-      denyButtonText: `Cancelar`,
-    }).then((result) => {
+
+  const eliminarFacultad = async (facultadId) => {
+    try {
+      const result = await Swal.fire({
+        title: "¿Estás seguro de eliminar esta Facultad?",
+        showDenyButton: true,
+        confirmButtonText: `Eliminar`,
+        denyButtonText: `Cancelar`,
+      });
+
       if (result.isConfirmed) {
-        setFacultades(facultades.filter((_, i) => i !== index));
-        toast.error("Facultad eliminada exitosamente!");
+        Swal.fire({
+          title: "Eliminando Facultad",
+          text: "Por favor, espera...",
+          icon: "info",
+          allowOutsideClick: false,
+          showConfirmButton: false,
+        });
+
+        const token = Cookies.get("token");
+        await axios.delete(
+          `${process.env.REACT_APP_API_URL}/faculties/${facultadId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        Swal.close();
+        Swal.fire("Facultad eliminada correctamente!", "", "info");
+        setFacultades(
+          facultades.filter((facultad) => facultad.id !== facultadId)
+        );
       }
-    });
+    } catch (error) {
+      console.error("Error al eliminar la facultad:", error);
+      toast.error("Error al eliminar la facultad");
+    }
+  };
+
+  const [loading, setLoading] = useState(true);
+  const [loadingFacultad, setLoadingFacultad] = useState(false); // Modificado: Inicializado en "false"
+
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [modalImagesIsOpen, setModalImagesIsOpen] = useState(false);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [defaultPageSize, setDefaultPageSize] = useState(5);
+
+  const openImagesModal = (images) => {
+    setSelectedImages(images);
+    setModalImagesIsOpen(true);
+  };
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      fetchFaculties(token);
+      fetchBuildings(token); // Llamada a la función para obtener la lista de edificios
+    }
+  }, []);
+
+  const fetchBuildings = async (token) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/buildings`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setEdificios(response.data);
+    } catch (error) {
+      console.error("Error fetching buildings:", error);
+    }
+  };
+
+  const fetchBuildingInfo = async (facultadId) => {
+    try {
+      const token = Cookies.get("token");
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/faculties/${facultadId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const buildingInfo = response.data.building;
+      return buildingInfo;
+    } catch (error) {
+      console.error("Error fetching building info:", error);
+      return null;
+    }
+  };
+  const fetchFaculties = async (token) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/faculties`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const facultadesWithBuildingInfo = await Promise.all(
+        response.data.map(async (facultad) => {
+          const buildingInfo = await fetchBuildingInfo(facultad.id);
+          return {
+            ...facultad,
+            edificioId: buildingInfo ? buildingInfo.id : null,
+            edificioNombre: buildingInfo ? buildingInfo.name : null,
+          };
+        })
+      );
+
+      setFacultades(facultadesWithBuildingInfo);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching buildings:", error);
+      setLoading(false);
+    }
   };
 
   const data = useMemo(() => facultades, [facultades]);
@@ -104,27 +259,52 @@ const Facultades = () => {
     () => [
       {
         Header: "Edificio",
-        accessor: "edificio",
+        accessor: "edificioNombre",
+        Cell: ({ value }) => <div>{value}</div>,
       },
       {
         Header: "Nombre",
-        accessor: "nombre",
+        accessor: "name",
+      },
+      {
+        Header: "Imagenes",
+        accessor: "imageUrls",
+        Cell: ({ value }) => (
+          <div>
+            {value.length > 0 ? (
+              <button onClick={() => openImagesModal(value)}>
+                Ver Imágenes
+              </button>
+            ) : (
+              <p className="requerido">Sin imágenes aún</p>
+            )}
+          </div>
+        ),
+      },
+      {
+        Header: "Descripcion",
+        accessor: "description",
+        Cell: ({ value }) => (
+          <div>
+            {value ? value : <p className="requerido">Sin Descripción aún</p>}
+          </div>
+        ),
       },
       {
         Header: "Acciones",
-        Cell: ({ row: { index } }) => (
+        Cell: ({ row: { original } }) => (
           <div>
             <button
               className="botonEyD"
               title="Editar"
-              onClick={() => editarFacultad(index)}
+              onClick={() => editarFacultad(original.id)}
             >
               <FontAwesomeIcon icon={faEdit} />
             </button>
             <button
               className="botonEyD"
               title="Eliminar"
-              onClick={() => eliminarFacultad(index)}
+              onClick={() => eliminarFacultad(original.id)}
             >
               <FontAwesomeIcon icon={faTrash} />
             </button>
@@ -135,15 +315,30 @@ const Facultades = () => {
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable(
-      {
-        columns,
-        data,
-        defaultColumn: { Filter: TextFilter },
-      },
-      useFilters
-    );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    canPreviousPage,
+    canNextPage,
+    nextPage,
+    previousPage,
+    pageCount,
+    gotoPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn: { Filter: TextFilter },
+      initialState: { pageIndex: pageNumber, pageSize: defaultPageSize },
+    },
+    useFilters,
+    usePagination
+  );
 
   return (
     <div className="facultades">
@@ -169,11 +364,16 @@ const Facultades = () => {
         <form onSubmit={handleSubmit(onSubmit)}>
           <label>
             Edificio:
-            <input
+            <select
               className="facultades input modalInput"
               {...register("edificio", { required: true })}
-              placeholder="Edificio al que pertenece"
-            />
+            >
+              {edificios.map((edificio) => (
+                <option key={edificio.id} value={edificio.id}>
+                  {edificio.name}
+                </option>
+              ))}
+            </select>
             {errors.edificio && (
               <p className="requerido">Este campo es requerido</p>
             )}
@@ -182,25 +382,51 @@ const Facultades = () => {
             Nombre:
             <input
               className="facultades input modalInput"
-              {...register("nombre", { required: true })}
+              {...register("nombre", {
+                required: true,
+                validate: (value) => value.trim().length > 3, // Validación para más de 3 letras
+              })}
               placeholder="Nombre"
             />
             {errors.nombre && (
+              <p className="requerido">El nombre debe tener más de 3 letras</p>
+            )}
+          </label>
+          <label>
+            Nombre:
+            <input
+              className="facultades input modalInput"
+              {...register("descripcion", { required: true })}
+              placeholder="Descripción"
+            />
+            {errors.descripcion && (
               <p className="requerido">Este campo es requerido</p>
             )}
           </label>
-
-          <div className="btnContainer">
-            <button type="submit" className="agregarBtn">
-              Agregar
-            </button>
-            <button
-              className="cancelarBtn"
-              onClick={() => setModalIsOpen(false)}
-            >
-              Cancelar
-            </button>
-          </div>
+          {loadingFacultad ? (
+            <div className="botones">
+              <ClipLoader
+                color="#3d8463"
+                loading={loadingFacultad}
+                size={"90px"}
+              />
+              <div style={{ fontSize: "30px" }}>Agregando Facultad...</div>
+            </div>
+          ) : (
+            <>
+              <div className="btnContainer">
+                <button type="submit" className="agregarBtn">
+                  Agregar
+                </button>
+                <button
+                  className="cancelarBtn"
+                  onClick={() => setModalIsOpen(false)}
+                >
+                  Cancelar
+                </button>
+              </div>
+            </>
+          )}
         </form>
       </Modal>
       <Modal
@@ -247,36 +473,129 @@ const Facultades = () => {
           </div>
         </form>
       </Modal>
+
+      <Modal
+        isOpen={modalImagesIsOpen}
+        onRequestClose={() => setModalImagesIsOpen(false)}
+        className="modalContent"
+        overlayClassName="modalOverlay"
+      >
+        <h2>Imágenes</h2>
+        {selectedImages.map((imageUrl, index) => (
+          <img
+            key={index}
+            src={imageUrl}
+            alt={`Imagen ${index + 1}`}
+            style={{
+              width: "50%",
+              height: "150px",
+              borderRadius: 0,
+              padding: 10,
+              margin: 0,
+            }}
+          />
+        ))}
+        <div className="botones2">
+          <button
+            className="cancelarBtn"
+            onClick={() => setModalImagesIsOpen(false)}
+          >
+            Cerrar
+          </button>
+        </div>
+      </Modal>
       <ToastContainer />
       <div className="tablafacultades">
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
-                    <div>
-                      {column.canFilter ? column.render("Filter") : null}
-                    </div>
-                  </th>
+        {loading ? (
+          <div className="botones">
+            <ClipLoader color="#3d8463" loading={loading} size={"90px"} />
+            <div style={{ fontSize: "30px" }}>
+              Actualizando Datos de la Tabla...
+            </div>
+          </div>
+        ) : (
+          <>
+            <table {...getTableProps()}>
+              <thead>
+                {headerGroups.map((headerGroup) => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column) => (
+                      <th {...column.getHeaderProps()}>
+                        {column.render("Header")}
+                        <div>
+                          {column.canFilter ? column.render("Filter") : null}
+                        </div>
+                      </th>
+                    ))}
+                  </tr>
                 ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {page.map((row) => {
+                  prepareRow(row);
+                  return (
+                    <tr {...row.getRowProps()}>
+                      {row.cells.map((cell) => (
+                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </>
+        )}
+      </div>
+      <div className="botones2">
+        <button
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+          className="pagination-button"
+        >
+          {"<"}
+        </button>{" "}
+        <button
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+          className="pagination-button"
+        >
+          {">"}
+        </button>{" "}
+        <span>
+          Página{" "}
+          <strong>
+            {pageIndex + 1} de {pageCount}
+          </strong>{" "}
+        </span>
+        <span>
+          | Ir a la página:{" "}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              let pageNumber = e.target.value ? Number(e.target.value) : 1;
+              pageNumber = Math.min(Math.max(pageNumber, 1), pageCount || 1);
+              pageNumber -= 1;
+              gotoPage(pageNumber);
+              setPageNumber(pageNumber);
+            }}
+            style={{ width: "75px" }}
+            min={1}
+            max={pageCount || 1}
+          />
+        </span>
+        <select
+          value={pageSize}
+          onChange={(e) => {
+            setPageSize(Number(e.target.value));
+          }}
+        >
+          {[5, 10, 20, 30, 40, 50].map((size) => (
+            <option key={size} value={size}>
+              Mostrar {size}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
